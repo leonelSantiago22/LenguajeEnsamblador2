120a727988eb6886cb7e3a89b58a0d06557ac2c6
leonelSantiago22 <santiago.rosas.leonel@gmail.com>
2022-04-27 21:34:20 -0500

    Tarea que entrega la cantidad de letras en una cadena

--- p2/14mpriat.asm
+++ p2/14mpriat.asm
@@ -5,13 +5,10 @@ include ..\fun\macros.asm
 .stack
 .code
 
-main:
-	print2 1,2,01000001b,"H"     ;parametros de cordenadas y parametros en binario que indican el color y el fondo
-	print2 1,3,11110100b,"O"
-	print2 1,4,00101011b,"L"
-	print2 1,5,00101011b,"A"
-	;print 1,1,00101011b," "
-
+main:	mov ax, @data
+		mov ds,ax 
+		mov ah,02h
+		int 21h
     print 'hola Mundo'
 	.exit 0
 end
---
+++ p2/taru8eo1.asm
@@ -0,0 +1,31 @@
+;Tarea 28/04/2022
+.MODEL SMALL
+.386
+include ..\fun\macros.asm
+extrn des4:near
+extrn desdec:near
+.STACK
+.DATA
+    cadena db "Hola Mundocc$"       ;inicializamos la cadena
+.CODE
+conteo  macro  direc,tam            ;definicion del macro
+local retorno,salida                ;Declarams como locales las variables para que no causen problemas
+            mov si,direc            ;apuntamos a la direccion de la cadena  que se recibio 
+            mov bx,0000h            ;inicializamos el apuntador
+            mov cx,50               ;ponemos como maximo de caracteres 50 caracteres
+retorno:    cmp byte ptr[si],24h    ;byte ptr nos ayuda a anivelar con 8 bits 
+            je salida               ;salta si es igual
+            inc si                  ;incrementamos la posicion del apuntador
+            inc bx                  ;incrementamos nuestro contador
+            loop retorno            ;loopeamos la funcion con maximo de 50 caracteres
+salida:     mov tam,bx              ;movemos nuestro contador a una direcion que en este caso es ax donde sera recibida
+endm                                ;fin del macro
+main:   mov ax,@data
+        mov ds,ax
+        mov es,ax                   ;habilitacion del extra segment
+        mov ax, offset cadena       ;cargamos la direcion de la cadena
+        conteo ax ax                ;mandamos a llamar el macro con la direcion de la cadena y la direcion donde recibiremos el dato
+        mov dx,ax                   ;desplegamos el resultado
+        call desdec                 ;mandamos a llamar el desdec
+        .exit 0
+end
