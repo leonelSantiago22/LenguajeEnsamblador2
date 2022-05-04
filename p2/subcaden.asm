774028acecaf2df52d694d44b16b3f85b822be27
leonelSantiago22 <santiago.rosas.leonel@gmail.com>
2022-05-04 13:50:15 -0500

    Se agregaron cambios a la subcadena

--- p2/subcaden.asm
+++ p2/subcaden.asm
@@ -1,28 +1,59 @@
 .model small
 include ..\fun\macros.asm
+extrn desdec:near
+extrn reto:near
+extrn des4:near
 .stack 
 .data
-    cadena1 db  "Estaba don gato sentado en una silla de palo"
-    cadena2 db  "una"
+        pre_cad db 2 dup(?)
+        cadena db  "Estaba bb una gato sentado bb una en silla de palo",0
+        subcad db  "bb",0
 .code 
 main:   mov ax,@data
         mov ds,ax 
         mov es,ax
-        mov si,offset cadena1
-        mov di,offset cadena2
-        mov dl,0000h
-        mov cl,0000h
-ini:    cmp si[dl], di[cl]
-        je iguales
-        inc dl
-        jmp ini 
-iguales: inc cl
-        cmp cl,3
-        je equals
-        jmp ini 
-        print 'no se encontro'
-        jmp salida
-equals: print 'se encontro' 
+        mov ax,0000h
+        mov dx,offset cadena
+        call strlen     ;saber el tamo de la cadena
+        push ax 
+        mov dx,offset subcad
+        call strlen
+        mov dh,al       ;tam cadena 
+        pop ax 
+        mov dl,al       ;tam subcad
+        call des4      ;desplegar los tamanos
+        call reto
+        cld 
+        mov bx,offset cadena
+c_sub:  cmp bx,44
+        jg salida
+        mov si,bx 
+        mov di, offset subcad
+        mov cx,2
+        repe cmpsb 
+        jne siguiente
+        print "cadena encontrada "
+        mov dx,bx
+        call desdec
+        inc bx
+        call reto
+        jmp c_sub
 
+siguiente:inc bx
+        jmp c_sub
 salida: .exit 0
+
+strlen: push bx 
+        push cx 
+        mov cl,0 
+        mov bx,dx 
+sl_c:   cmp byte ptr[bx],0
+        je sl_s
+        inc bx 
+        inc cl 
+        jmp sl_c
+sl_s:   mov al,cl
+        pop cx 
+        pop bx
+        ret
 end
