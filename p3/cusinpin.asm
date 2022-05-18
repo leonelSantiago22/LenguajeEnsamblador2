88bc3219dfa107988fc9d9e17156aaeb98433560
leonelSantiago22 <santiago.rosas.leonel@gmail.com>
2022-05-18 13:03:55 -0500

    Imprimir un cuadro

--- p3/cusinpin.asm
+++ p3/cusinpin.asm
@@ -1,9 +1,10 @@
+;Cuadro sin pintar
 .model small
 .386
 .stack
 .data
 .code
-pintar macro x,y,color,cant
+pintarx macro x,y,color,cant
 local cic 
     pusha
         mov cx,x ;Coordenada X.
@@ -14,6 +15,20 @@ cic:    mov dx,y ;Coordenada Y.
         ;Esperar usuario (para que no termine sin poder ver)
         inc cx
         cmp cx,cant
+        jle cic
+    popa    
+endm
+pintary macro x,y,color,cant
+local cic 
+    pusha
+        mov cx,x ;Coordenada X.
+        mov al,color ;Amarillo.
+        mov ah,0Ch ;Funcion escribir punto.
+cic:    mov dx,y ;Coordenada Y.
+        int 10h 
+        ;Esperar usuario (para que no termine sin poder ver)
+        inc dx
+        cmp dx,cant
         jle cic
     popa    
 endm
@@ -30,12 +45,12 @@ main: ;Obtener configuración de pantalla y guardarla en la pila .
         mov al,0Fh
         ;desplegar algo
         ;x y color tam
-        mov si,0100
-cic:    pintar cx dx al 0400
-        inc dx 
-        inc si
-        cmp si,0250
-        jle cic
+        ;mov si,0100
+        pintarx cx dx al 0400
+        pintary 0400 dx al 0400
+        pintary cx dx al 0400
+        mov dx,0400
+        pintarx cx dx al 0400 
         ;esperar usuario
         mov ah,00h
         int 16h
